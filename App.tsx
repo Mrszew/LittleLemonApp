7f852fd1f78ae32a2a6a33adaf72929d4025ec43
Marcel Raszewski <marcelraszewski@MacBook-Air-Marcel.local>
2024-07-30 20:53:29 +0200

    new comit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { Routes } from "./navigation/Routes";
+import MainNavigation from "./navigation/mainNavigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return(
+    <NavigationContainer>
+        <MainNavigation>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        </MainNavigation>
+    </NavigationContainer>
+  )
+};
+
+export default App;
